afd3df747e081bde27c1db78f95bdb2a9f41376d
Greg <gregvjames99@gmail.com>
2020-12-01 15:48:15 -0600

    PSC13

--- optical_simulation/cudaKernels.py
+++ optical_simulation/cudaKernels.py
@@ -39,7 +39,7 @@ def SrcPointCalc(point, pos, GratingSeparation, obsPoints, sourcePoints, WaveNum
   return (phase, U)
 
 # This function gets called for every observation point
-@jit
+@cuda.jit
 def intensityKernel(GratingSeparation, WaveNumber, sourcePoints, obsPoints, sourceAmp, sourcePhase, out_phase, out_amp,
                     out_intense):
     """calculates intensity, amplitude and phases between sources points and observation points
